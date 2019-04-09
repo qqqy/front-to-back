147e30fa7ee2c1d8e1e86450377a7ba9c2dce2b4
qqqy <frogasaurus@hotmail.com>
2019-04-09 10:38:21 -0600

    moved console.logs to a separate function.

--- src/App.js
+++ src/App.js
@@ -12,14 +12,21 @@ class App extends Component {
     //CODE HERE//
 
     //END CODE//
-    console.log("ALL: " , this.state.all)
-    console.log("QUERY: " , this.state.query)
-    console.log("PARAM: " , this.state.param)
   }
+
+  checkSolution = () => {
+    console.log("ALL: " , this.state.all);
+    console.log("QUERY: " , this.state.query);
+    console.log("PARAM: " , this.state.param);
+  }
+  
+  
   render() {
     return (
       <div className="App">
         <h1>OPEN THE CONSOLE</h1>
+        <button onClick={this.checkSolution}>Check Solution</button>
+
       </div>
     );
   }
